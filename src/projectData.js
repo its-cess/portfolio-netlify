73b51df68b18a222c0325edc604472b88e48bc4f
Cecily Toro <torocecily@gmail.com>
2022-10-07 12:27:59 -0400

    update folder for project images. add clothing store project.

--- src/projectData.js
+++ src/projectData.js
@@ -1,10 +1,21 @@
 let projects = [
   {
+    id: 12,
+    title: "Clothing Store",
+    description:
+      "This fake clothing store allows users to sign up, sign in, and shop. Customers can add things to their cart and get an overview of their cart from the cart dropdown menu. From the checkout page, customers can increase or decrease the quantity of items or remove them from the cart completely. The cart total is adjusted accordingly.",
+    image: "projects/clothing-store.png",
+    url: "https://sensational-belekoy-1a394b.netlify.app",
+
+    using:
+      "This project was created using React v18, React Router v6, SASS, and Styled Components. Firebase is used as a database and for authentication. Authentication is available using email/password or with the Google Sign in Method. It is hosted on Netlify."
+  },
+  {
     id: 11,
     title: "MERN app",
     description:
       "This project allows users to create an account, log in, and add their favorite places. Authentication and authorization are in place along with form validation for signing up, logging in, and creating a new place. Certain routes and actions are disabled based on the logged in user. Only the user that created the place is allowed to edit or delete it. A pop up modal allows users to view the locations on a map. Image upload is used for both profile pictures and place images. This app uses all of the CRUD operations.",
-    image: "/mern-project.png",
+    image: "projects/mern-project.png",
     url: "https://vue-http-demo-df59d.web.app/",
 
     using:
@@ -15,7 +26,7 @@ let projects = [
     title: "Blog",
     description:
       "This project allows users to login and create blog posts. All CRUD operations are available as users are able to also edit and delete their blog posts when logged in.",
-    image: "/blog.png",
+    image: "projects/blog.png",
     url: "https://www.loom.com/share/a21002c7293e44588904a7b4149f81c9",
     repo: "https://github.com/its-cess/blog",
     using:
@@ -25,7 +36,7 @@ let projects = [
     id: 9,
     title: "Monster Rolodex",
     description: `This project fetches monster data and displays them in a monsters "rolodex". Users are able to search and filter out monsters by typing in the search field.`,
-    image: "/monsters-rolodex.png",
+    image: "projects/monsters-rolodex.png",
     url: "https://its-cess.github.io/react-classes-monsters/",
     repo: "https://github.com/its-cess/react-classes-monsters",
     using:
@@ -35,7 +46,7 @@ let projects = [
     id: 8,
     title: "Resource Library",
     description: `This application allows you to create and store resources. Validation is used when adding a resource and a modal will popup if not all fields are added. Clicking on "View Resource" will take you to the page of the link provided. Users are also able to delete resources.`,
-    image: "/resource-library.png",
+    image: "projects/resource-library.png",
     url: "https://its-cess.github.io/resource-library/",
     repo: "https://github.com/its-cess/resource-library",
     using: "This project was created using Vue3 and CSS."
@@ -45,7 +56,7 @@ let projects = [
     title: "Monster Slayer Game",
     description:
       "Users get to play against the computer to try to slay the monster. Users can choose to attack, heal themselves, use a special attack, or surrender. The monster's and player's health are displayed on screen. Special attacks are only available every third turn. Surrender automatically ends the game and the player loses. When a player's health runs out, the monster wins. When the monster's health runs out, the player wins. A battle log is output on the screen displaying each move that is made by the player and monster. The battle log also displays how much damage or healing occurred.",
-    image: "/monster-slayer.png",
+    image: "projects/monster-slayer.png",
     url: "https://its-cess.github.io/monster-slayer/",
     repo: "https://github.com/its-cess/monster-slayer",
     using: "This game was created using Vue.js and CSS."
@@ -55,7 +66,7 @@ let projects = [
     title: "Recipes",
     description:
       "This site is used for storing recipes. You can view the recipe and return to the top of the page when finished. There is a home button to take you back to the listing of all the recipes. For multi-part recipes, a carousel is used to separate the ingredients by section.",
-    image: "/recipes.png",
+    image: "projects/recipes.png",
     url: "https://its-cess.github.io/odin-recipes/recipes/",
     repo: "https://github.com/its-cess/odin-recipes",
     using: "This site was created using HTML, CSS, and Bootstrap."
@@ -65,7 +76,7 @@ let projects = [
     title: "To-Do List",
     description:
       "This is an interactive to-do list. You can add items, check them off the list when complete, and clear the list when everything is done. If everything is not finished, the items will be stored and can be finished at another time. Items will only be removed when the list is cleared.",
-    image: "/to-do-list.png",
+    image: "projects/to-do-list.png",
     url: "https://its-cess.github.io/to-do-list/",
     repo: "https://github.com/its-cess/to-do-list",
     using:
@@ -76,7 +87,7 @@ let projects = [
     title: "Tindog",
     description:
       "This is webpage for a fake startup called TinDog (Tinder for dogs!). Links in the nav bar will take you to the corresponding section. A carousel is used to display the fake reviews of this app.",
-    image: "/tindog.png",
+    image: "projects/tindog.png",
     url: "https://its-cess.github.io/tindog/",
     repo: "https://github.com/its-cess/tindog",
     using: "This page was created using HTML, CSS, and Bootstrap."
@@ -86,7 +97,7 @@ let projects = [
     title: "Simon Says",
     description:
       "The game starts when a player pushes any key. The computer picks a color and a user has to click the matching color. Every round the pattern gets longer and the user has to repeat the pattern in the correct order or they lose. Different sounds are played with each color pressed. The level is shown at the top and increases each round the player gets correct. When a player gets the pattern incorrect, the screen flashes red and returns them back to Level 1.",
-    image: "/simon-game.png",
+    image: "projects/simon-game.png",
     url: "https://its-cess.github.io/simon-game/",
     repo: "https://github.com/its-cess/simon-game",
     using: "This game was created using HTML, CSS, JavaScript, and jQuery."
@@ -96,7 +107,7 @@ let projects = [
     title: "Drum Set",
     description:
       "This game allows users to play the drums online! A different sound is played with each corresponding piece of the drumset. The user can either click on the images to play the sound, or press the corresponding key.",
-    image: "/drum-kit.png",
+    image: "projects/drum-kit.png",
     url: "https://its-cess.github.io/drum-kit/",
     repo: "https://github.com/its-cess/drum-kit",
     using: "This game was created with HTML, CSS, and vanilla JavaScript."
@@ -105,7 +116,7 @@ let projects = [
     id: 1,
     title: "Dicee Game",
     description: `This game shows two dice, one for Player 1 and another for Player 2. On refresh, the dice are "rolled" and whichever player has the higher number wins. The winner is displayed at the top of the screen. If the players roll the same number, a tie occurs.`,
-    image: "/dicee.png",
+    image: "projects/dicee.png",
     url: "https://its-cess.github.io/dice-game/",
     repo: "https://github.com/its-cess/dice-game",
     using: "This game was created with HTML, CSS and vanilla JavaScript."
